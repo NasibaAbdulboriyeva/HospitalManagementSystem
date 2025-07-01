4c7c2172aad3fa79183f8be12ab3e6b4dbc473f9
Po'lat <Po'lat@DEVELOPER>
2025-07-01 09:02:28 +0500

    Create User Entity

---
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using HospitalManagementSystem.Domain.Enums;
+
+namespace HospitalManagementSystem.Domain.Entities;
+
+public class User
+{
+    public long UserId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    public string Email { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string Username { get; set; }
+
+    public string PasswordHash { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+
+    public Gender? Gender { get; set; }
+
+    public string? ProfileImageUrl { get; set; }
+
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
