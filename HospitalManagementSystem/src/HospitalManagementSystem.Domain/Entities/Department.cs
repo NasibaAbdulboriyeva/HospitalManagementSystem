db1d034fb889719caeec7243ea77115c78fe0c1c
baxodirovF <farxod.baxodirov@webase.uz>
2025-07-02 05:15:51 +0500

    Add Department entity with navigation properties

---
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace HospitalManagementSystem.Domain.Entities;
+
+public class Department
+{
+    public long DepartmentId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    //Navigation properties 
+    //public ICollection<Room> Rooms { get; set; }
+    //public ICollection<Doctor> Doctors { get; set; } 
+    //public ICollection<Nurse> Nurses { get; set; } 
+}
+
