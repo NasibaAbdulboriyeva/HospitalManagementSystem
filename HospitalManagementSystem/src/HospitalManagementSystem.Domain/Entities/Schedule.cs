332100803dcda6d98702e248b8ffd542d7c073fe
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-07-04 07:44:11 +0500

    feat(domain): create Schedule entity

---
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/Schedule.cs
@@ -0,0 +1,20 @@
+﻿namespace HospitalManagementSystem.Domain.Entities;
+
+public class Schedule
+{
+    public long ScheduleId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public bool IsOnCall { get; set; }
+    public enum DayOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    public long UserId { get; set; }
+}
