c81ddf48fa7779297ad3381469a3df43b1d68342
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-07-10 22:50:09 +0500

    Notification and Feedback entities added

---
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/Feedback.cs
@@ -0,0 +1,15 @@
+﻿namespace HospitalManagementSystem.Domain.Entities
+{
+    public class Feedback : IAuditEntity
+    {
+        public long FeedbackId { get; set; }
+
+        public long PatientId { get; set; }
+
+        public string Message { get; set; }
+
+        public int Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+    }
+}
