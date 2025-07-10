1016639950ecc17504148ecddd45395884c77fa2
baxodirovF <farxod.baxodirov@webase.uz>
2025-07-10 22:13:12 +0500

    add Prescription entity to record doctor-prescribed medications for patients

---
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/Prescription.cs
@@ -0,0 +1,25 @@
+﻿namespace HospitalManagementSystem.Domain.Entities;
+
+public class Prescription : IAuditEntity
+{
+    public long PrescriptionId { get; set; }
+
+    public long PatientId { get; set; }
+    public Patient Patient { get; set; } 
+
+    public long DoctorId { get; set; }
+    public Doctor Doctor { get; set; } 
+
+    public DateTime DatePrescribed { get; set; }
+
+    public string MedicineName { get; set; } 
+
+    public string Dosage { get; set; } 
+
+    public int DurationDays { get; set; }
+
+    public string Instructions { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+}
