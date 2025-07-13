04e7ac013ad9c61e7e98d5e2ac357639406b3775
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-07-13 18:14:56 +0500

    UserConfiguration added

--- HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/UserRole.cs
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/UserRole.cs
@@ -3,7 +3,9 @@
 public class UserRole : IAuditEntity
 {
     public long UserId { get; set; }
+    public User User { get; set; }
     public long RoleId { get; set; }
+    public Role Role { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? LastModifiedAt { get; set; }
 }
