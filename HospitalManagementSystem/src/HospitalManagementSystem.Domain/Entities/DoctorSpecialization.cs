66fdbe9ecbbdd0f4b41744ded42ca705cfc00875
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-07-14 10:10:03 +0500

    UserRole

--- HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/DoctorSpecialization.cs
+++ HospitalManagementSystem/src/HospitalManagementSystem.Domain/Entities/DoctorSpecialization.cs
@@ -3,7 +3,9 @@
 public class DoctorSpecialization : IAuditEntity
 {
     public long DoctorId { get; set; }
+    public Doctor Doctor { get; set; }
     public long SpecializationId { get; set; }
+    public Specialization Specialization { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? LastModifiedAt { get; set; }
 }
